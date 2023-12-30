465e2375027a57758829d565a5a329b339c54797
AbdouAI <contact.abdou03@gmail.com>
2023-12-30 17:58:11 +0100

    updated routes location

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -49,7 +49,7 @@ const App=()=>{
         <BrowserRouter>
             <div className={isDark?"app-container dark":"app-container"}>
                 <Header childClick={changeLumMode} isDark={isDark}/>
-                <Routes>
+                <Routes location="/">
                     <Route path="/" element={countryList===null && !failedFetch?<Loading/>:failedFetch?<p style={{fontSize:"1.5rem",padding:"2rem"}}>Error:Make sure you are connected to the internet</p>:<Home countryList={countryList} isDark={isDark}/>}/>
                     <Route path="/country/:code" element={<CountryDetails countryList={countryList}  isDark={isDark}/> } key={window.location.pathname}/>
                 </Routes>
