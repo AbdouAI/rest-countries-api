450cbd53d2577039606b2551ba024c49b218fc57
AbdouAI <contact.abdou03@gmail.com>
2023-12-30 16:46:19 +0100

    routes fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/rest-countries-api/"
+  base:"/rest-countries-api/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
